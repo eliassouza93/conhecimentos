24d5404231ac831e0b1d5dfc91d5d07bee231c43
Elias de Souza <elias99-souza@hotmail.com>
2022-11-08 11:31:48 -0300

    finalizandoProps

--- conhecimentos/src/components/AmbienteLista/Formulario/index.tsx
+++ conhecimentos/src/components/AmbienteLista/Formulario/index.tsx
@@ -7,24 +7,33 @@ interface IPessoa {
 
 export default function Formulario({ setLista }: { setLista: React.Dispatch<React.SetStateAction<IPessoa[]>> }) {
     const [nome, setNome] = useState('')
-    const [idade, setIdade] = useState('00')
+    const [idade, setIdade] = useState('')
 
     const tudo: any = {
         nome,
         idade
     }
+    
     function Adicionar(e: { preventDefault: () => void }) {
         e.preventDefault()
-
         setLista(antigas => [...antigas, { ...tudo }])
-       
+
+        if (Adicionar == Adicionar) {
+            setNome('')
+        }
+        console.log(nome)
+    }
+    function MostrarNomes() {
+        
     }
 
-    return (
-        <form>
-            <input placeholder="digite seu nome" onChange={(e) => setNome(e.target.value)} type="text" name="nome" value={tudo.nome} />
 
-            <button onClick={Adicionar}>Adicionar</button>
+    return (
+        <form onSubmit={Adicionar}>
+            <input placeholder="digite seu nome" onChange={(e) => setNome(e.target.value)} type="text" name="nome" value={tudo.nome} autoFocus />
+
+            <button type="submit"  >Adicionar</button>
+          
         </form>
     )
 }
