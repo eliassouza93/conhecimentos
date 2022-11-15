47c07c82f59b2ca60865c88a0979d71718a0e4b7
Elias de Souza <elias99-souza@hotmail.com>
2022-11-15 16:27:42 -0300

    restaurante_api

--- conhecimentos/src/components/API/Inicio/index.tsx
+++ conhecimentos/src/components/API/Inicio/index.tsx
@@ -4,6 +4,7 @@ import { Fragment, useEffect, useState } from "react"
 
 export default function InicioApi() {
     const [carregando, setCarregando] = useState(false)
+    
     const [usuario, setUsuario] = useState([])
 
 
@@ -11,8 +12,6 @@ export default function InicioApi() {
         setCarregando(true)
         const time = setTimeout(() => {
  
-
-             
             axios.get('https://api.themoviedb.org/3/movie/popular?api_key=aed2ce3dba886fed0e5f20d0e13feac5&language=pt-br-US&page=1')
                 .then((resposta) => {
                     setUsuario(resposta.data.results)
@@ -27,7 +26,7 @@ export default function InicioApi() {
 
         }, 2200);
 
-     
+
 
 
     }, [])
--- conhecimentos/src/index.tsx
+++ conhecimentos/src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css'
-import InicioC from 'componentes/TestandoLIBs/Inicio';
- 
-
-
-
-
+import PaginaIndex from 'componentes1';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <InicioC />
+    <PaginaIndex />
   </React.StrictMode>
 );
 
