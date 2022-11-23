c08b0d275a4d211ceaf8a0aacd8bc033b9bb1d61
Elias de Souza <elias99-souza@hotmail.com>
2022-11-23 12:41:40 -0300

    node_v18

--- conhecimentos/src/componentes/TestandoLIBs/Inicio/index.tsx
+++ conhecimentos/src/componentes/TestandoLIBs/Inicio/index.tsx
@@ -3,6 +3,7 @@ import CarouselPagina from "componentes/TestandoLIBs/Carousel";
 export default function InicioC() {
     return (
         <div>
+            <h1>Pagina</h1>
             <CarouselPagina />
         </div>
     )
