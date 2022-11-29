7f0d59f7428808449a832e071731ca8e31ab0113
Elias de Souza <elias99-souza@hotmail.com>
2022-11-29 08:53:02 -0300

    apiRestJson

--- conhecimentos/src/components/Pokemon/index.tsx
+++ conhecimentos/src/components/Pokemon/index.tsx
@@ -1,26 +1,9 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
 
 export default function Pokemon() {
-    const [dados, setDados] = useState<any[]>([])
-
-    useEffect(() => {
-        axios.get(' https://pokeapi.co/api/v2/pokemon/ditto')
-            .then((resposta) => {
-                setDados(resposta.data.abilities)
-            })
-    }, [])
 
 
     return (
         <div>
-            <h1>Consumindo Pokemon API</h1>
-            {dados.map((exibe, index) => (
-                <div key={index} >
-                    <h4> {exibe.ability.name} </h4>
-                    
-                </div>
-            ))}
 
         </div>
     )
